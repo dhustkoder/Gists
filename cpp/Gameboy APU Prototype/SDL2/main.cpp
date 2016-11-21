345d7f77f43ec34ab52ebdd608edac121a4bc40c
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-11-21 19:00:33 -0200

    APU Prototype

--- cpp/Gameboy APU Prototype/SDL2/main.cpp
+++ cpp/Gameboy APU Prototype/SDL2/main.cpp
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <SDL2/SDL.h>
+
 #include "finally.hpp"
+#include "apu.hpp"
 
 
 enum class WaveType : uint8_t {
@@ -18,52 +20,7 @@ struct AudioData {
 	WaveType wave_type;
 };
 
-struct Apu {
-	struct {
-		union {
-			uint8_t value;
-			struct {
-				uint8_t sweep_shift : 3;
-				uint8_t sweep_inc_dec : 1;
-				uint8_t sweep_time : 3;
-			};
-		} nr10;
 
-
-		union {
-			uint8_t value;
-			struct {
-				uint8_t length_data : 6;
-				uint8_t wave_pattern : 2;
-			};
-		} nr11;
-
-		union {
-			uint8_t value;
-			struct {
-				uint8_t envelop_sweep : 3;
-				uint8_t envelop_dir : 1;
-				uint8_t initial_vol : 4;
-			};
-		} nr12;
-
-		union {
-			uint8_t freq_lower_bits;
-		} nr13;
-
-		union {
-			uint8_t value;
-			struct {
-				uint8_t freq_higher_bits : 3;
-				uint8_t dummy : 3;
-				uint8_t counter : 1;
-				uint8_t inital : 1;
-			};
-		} nr14;
-
-	} channel_1;
-
-};
 
 
 static bool init_sdl();
