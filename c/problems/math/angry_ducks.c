572e81291317978b7385b08c691c04bc4705e4a1
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-11-29 12:34:23 -0200

    angry ducks

--- c/problems/math/angry_ducks.c
+++ c/problems/math/angry_ducks.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	
+	const long double g = 9.80665;
+	const long double pi = 3.14159;
+	long double high, angle, speed, rad, aux1, aux2, res;
+	int i, n, p1, p2;
+	while (scanf("%Lf %d %d %d", &high, &p1, &p2, &n) != EOF) {
+		aux1 = (2 * high) / g;
+		for (i = 0; i < n; ++i) {
+			scanf("%Lf %Lf", &angle, &speed);
+			rad = (angle / 180.00) * pi;
+			aux2 = (speed * sinl(rad)) / g;
+			res = (speed * cosl(rad)) * (sqrtl(aux1 + powl(aux2, 2)) + aux2);
+			printf("%.5Lf -> %cUCK\n", res,
+			       (res >= p1 && res <= p2) ? 'D' : 'N');
+		}
+	}
+
+	return 0;
 }
 
--- c/problems/math/compile.sh
+++ c/problems/math/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$CC" != "" ]; then
-	$CC -std=c90 -Wall -Wextra -pedantic -O2 -lm -o "${1%".c"}.out" $1
+	$CC -Wall -Wextra -pedantic -O2 -lm -o "${1%".c"}.out" $1
 else
 	echo "Set your C compiler by CC environment variable. Example: export CC=gcc"
 fi
