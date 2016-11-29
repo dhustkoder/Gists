7b8e9062a1a9ecc47202c1c59b1b37cfa130e42b
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-11-29 06:50:30 -0200

    logic seq 1

---
+++ c/problems/math/logic_seq_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n, i, aux = 1;
+	scanf("%d", &n);
+	n *= 2;
+	for (i = 0; i < n; ++i) {
+		if ((i % 2) == 0) {
+			printf("%.0f %.0f %.0f\n", pow(aux, 1), 
+			        pow(aux, 2), pow(aux, 3));
+		} else {
+			printf("%.0f %.0f %.0f\n", pow(aux, 1), 
+			        pow(aux, 2) + 1, pow(aux, 3) + 1);
+			++aux;
+		}
+	}
+
+	return 0;
+}
+
