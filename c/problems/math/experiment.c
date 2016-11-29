584ac0130095e7cc01ebb4fe9b62351a81a6fbea
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-11-29 07:29:57 -0200

    experiment added

---
+++ c/problems/math/experiment.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+	int ninput, i, n;
+	int total_animals;
+	int rabbits = 0, rats = 0, frogs = 0;
+	char type;
+
+	scanf("%d", &ninput);
+	for (i = 0; i < ninput; ++i) {
+		scanf("%d %c", &n, &type);
+		if (type == 'C')
+			rabbits += n;
+		else if (type == 'R')
+			rats += n;
+		else if (type == 'S')
+			frogs += n;
+	}
+	
+	total_animals = rabbits + rats + frogs;
+
+	printf("Total: %d cobaias\n"
+	       "Total de coelhos: %d\n"
+	       "Total de ratos: %d\n"
+	       "Total de sapos: %d\n"
+	       "Percentual de coelhos: %.2f %%\n"
+	       "Percentual de ratos: %.2f %%\n"
+	       "Percentual de sapos: %.2f %%\n",
+	       total_animals, rabbits, rats, frogs,
+	       (100.00 * rabbits) / total_animals,
+	       (100.00 * rats) / total_animals,
+	       (100.00 * frogs) / total_animals);
+
+	return 0;
+}
+
