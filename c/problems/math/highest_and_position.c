86f536c4ca3fb5d5b09651aa955daefa1bfe0ab1
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-11-29 06:23:51 -0200

    more problems solved

---
+++ c/problems/math/highest_and_position.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int highest = 0, pos = 0, input, i;
+	for (i = 1; i <= 100; ++i) {
+		scanf("%d", &input);
+		if (input > highest) {
+			highest = input;
+			pos = i;
+		}
+	}
+
+	printf("%d\n%d\n", highest, pos);
+	return 0;
+}
+
