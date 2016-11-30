3aaccfc97d049ffe078643385b0154d897492c17
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-11-30 04:11:06 -0200

    maester's map revision

--- cpp/problems/arrays/maesters_map.cpp
+++ cpp/problems/arrays/maesters_map.cpp
@@ -3,11 +3,18 @@
 
 int main()
 {
-	int x, y;
+	const auto read_int = [] {
+		int i;
+		std::cin >> i;
+		return i;
+	};
+
+	const int width = read_int();
+	const int height = read_int();
 	int known_offsets_sz = 0;
-	std::cin >> x >> y;
-	char* const vec = new char[(x + 1) * y];
-	int* const known_offsets = new int[x * y];
+
+	char* const vec = new char[(width + 1) * height];
+	int* const known_offsets = new int[width * height];
 	
 	const auto is_known_offset = 
 	[&known_offsets_sz, known_offsets](const int offset) {
@@ -15,44 +22,42 @@ int main()
 		const auto end = begin + known_offsets_sz;
 		return std::find(begin, end, offset) != end;
 	};
+
 	const auto add_known_offset =
 	[&known_offsets_sz, known_offsets](const int offset) {
 		known_offsets[known_offsets_sz++] = offset;
 	};
 
-	for (int i = 0; i < y; ++i)
-		std::cin >> &vec[i * x];
+	for (int y = 0; y < height; ++y)
+		std::cin >> &vec[y * width];
 
-	int next_x = 0, next_y = 0;
+	int x = 0, y = 0;
 	bool found = false;
-	char last_char = 0, new_char = 0;
+	char last_char = 0;
 
 	do {
-		const int offset = (next_y * x) + next_x;
+		const int offset = (y * width) + x;
 		const char map_char = vec[offset];
-		const char check_char = 
-		  map_char == '.' ? last_char : (new_char = map_char);
+		const char check_char = map_char == '.' ? last_char : map_char;
 
-		if (last_char != new_char) {
+		if (last_char != check_char) {
 			if (is_known_offset(offset)) {
 				break;
 			} else {
 				add_known_offset(offset);
-				last_char = new_char;
+				last_char = check_char;
 			}
 		}
 
 		switch (check_char) {
-		case '>': ++next_x; break;
-		case 'v': ++next_y; break;
-		case '<': --next_x; break;
-		case '^': --next_y; break;
+		case '>': ++x; break;
+		case 'v': ++y; break;
+		case '<': --x; break;
+		case '^': --y; break;
 		case '*': found = true; break;
 		}
 
-	} while (!found && 
-	        (next_x >= 0 && next_x < x) && 
-	        (next_y >= 0 && next_y < y));
+	} while (!found && x >= 0 && x < width && y >= 0 && y < height);
 
 	if (found) 
 		std::cout << "*\n";
