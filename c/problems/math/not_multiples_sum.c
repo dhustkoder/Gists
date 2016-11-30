6002d4c09dd43faf65a135275ec3a25690d6da51
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-11-30 09:31:41 -0200

    not multiples of 13 sum

---
+++ c/problems/math/not_multiples_sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	long x, y, begin, end, i;
+	long sum = 0;
+	scanf("%ld %ld", &x, &y);
+	
+	if (x > y) {
+		begin = y;
+		end = x + 1;
+	} else {
+		begin = x;
+		end = y + 1;
+	}
+
+	for (i = begin; i < end; ++i) {
+		if ((i % 13) != 0)
+			sum += i;
+	}
+
+	printf("%ld\n", sum);
+	return 0;
+}
