029b1b47b0df365cc1b172a6c1bc79c00027e118
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-12-01 06:46:21 -0200

    collectable cards

---
+++ c/problems/math/collectable_cards.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int ninput, a, b, q, r, i;
+	scanf("%d", &ninput);
+
+	for (i = 0; i < ninput; ++i) {
+		scanf("%d %d", &a, &b);	
+		
+		while (b != 0) {
+			q = a / b;
+			r = a - b * q;
+			a = b;
+			b = r;
+		}
+
+		printf("%d\n", a);
+	}
+
+	return 0;
+}
+
+
