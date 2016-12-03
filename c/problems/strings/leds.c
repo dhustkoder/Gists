863d1fe1e9114a106d0d67be4552d461e13e38db
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-12-03 09:53:18 -0200

    leds and new problems section: strings

---
+++ c/problems/strings/leds.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	const int leds_num[10] = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
+	char buffer[110];
+	int i, j, ninput;
+	long sum;
+	
+	scanf("%d", &ninput);
+
+	for (i = 0; i < ninput; ++i) {
+		scanf("%s", buffer);
+		
+		for (j = 0, sum = 0; buffer[j] != '\0'; ++j)
+			sum += leds_num[buffer[j] - '0'];
+
+		printf("%ld leds\n", sum);
+	}
+
+	return 0;
+}
+
