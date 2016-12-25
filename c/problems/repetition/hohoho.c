609f74265bb624e518beb4c563496c7da41383ab
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-12-25 21:24:11 -0200

    hohoho improved

--- c/problems/repetition/hohoho.c
+++ c/problems/repetition/hohoho.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+inline void write_ho(char** itr)
+{
+	**itr = 'H';
+	++(*itr);
+	**itr = 'o';
+	++(*itr);
+}
 
 int main(void)
 {
 	long n, i;
 	scanf("%ld", &n);
-	for (i = 0; i < (n-1); ++i)
-		printf("Ho ");
-	printf("Ho!\n");
+	char* const buffer = malloc(sizeof(char) * (n * 3) + 2);
+	char* itr = buffer;
+
+	for (i = 0; i < (n - 1); ++i) {
+		write_ho(&itr);
+		*itr++ = ' ';
+	}
+
+	write_ho(&itr);
+	*itr++ = '!';
+	*itr++ = '\0';
+
+	puts(buffer);
+	free(buffer);
 	return 0;
 }
+
