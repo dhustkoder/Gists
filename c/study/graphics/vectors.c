bef9f74053260d63ad106b3f51d753b23356757d
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-01-07 00:00:05 +0000

    refact vectors study

--- c/study/graphics/vectors.c
+++ c/study/graphics/vectors.c
@@ -1,22 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
 typedef struct Vector2f {
 	float x, y;
 } vec2f_t;
 
 
-inline float get_magnitude(const vec2f_t* const v)
+inline vec2f_t add_vec2f(const vec2f_t a, const vec2f_t b)
 {
-	return sqrtf((v->x * v->x) + (v->y * v->y));
-}
-
-inline vec2f_t get_diff(const vec2f_t* const from, const vec2f_t* const to)
-{
-	const vec2f_t result = { to->x - from->x, to->y - from->y };
+	const vec2f_t result = { a.x + b.x, a.y + b.y };
 	return result;
 }
+
+
+
+inline vec2f_t sub_vec2f(const vec2f_t a, const vec2f_t b)
+{
+	const vec2f_t result = { a.x - b.x, a.y - b.y };
+	return result;
+}
+
+
+
+inline vec2f_t unit_vec2f(const vec2f_t v, const float mag)
+{
+	const vec2f_t result = { v.x / mag, v.y / mag };
+	return result;
+}
+
+
+
+inline vec2f_t neg_vec2f(const vec2f_t v)
+{
+	const vec2f_t result = { -v.x, -v.y };
+	return result;
+}
+
+
+
+inline float mag_vec2f(const vec2f_t v)
+{
+	return sqrtf((v.x * v.x) + (v.y * v.y));
+}
+
+
+
+inline vec2f_t diff_vec2f(const vec2f_t from, const vec2f_t to)
+{
+	return sub_vec2f(to, from);
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -28,13 +64,17 @@ int main(int argc, char** argv)
 
 	const vec2f_t a = { strtof(argv[1], NULL), strtof(argv[2], NULL) };
 	const vec2f_t b = { strtof(argv[3], NULL), strtof(argv[4], NULL) };
+	const vec2f_t ab_sum = add_vec2f(a, b);
+	const vec2f_t ab_sub = sub_vec2f(a, b);
 
-	const vec2f_t a_to_b_diff = get_diff(&a, &b);
-	const vec2f_t b_to_a_diff = { -a_to_b_diff.x, -a_to_b_diff.y };
+	const vec2f_t diff = diff_vec2f(a, b);
+	const vec2f_t negdiff = neg_vec2f(diff);
 
-	const float a_magnitude = get_magnitude(&a);
-	const float b_magnitude = get_magnitude(&b);
-	const float diff_magnitude = get_magnitude(&a_to_b_diff);
+	const float a_mag = mag_vec2f(a);
+	const float b_mag = mag_vec2f(b);
+	const float diff_mag = mag_vec2f(diff);
+	const vec2f_t a_unit = unit_vec2f(a, a_mag);
+	const vec2f_t b_unit = unit_vec2f(b, b_mag);
 
 	printf("a => (%.2f, %.2f)\n"
 	       "b => (%.2f, %.2f)\n"
@@ -42,14 +82,21 @@ int main(int argc, char** argv)
 	       "b magnitude => %.2f\n"
 	       "a to b diff => (%.2f, %.2f)\n"
 	       "b to a diff => (%.2f, %.2f)\n"
-	       "diff magnitude => %.2f\n",
+	       "diff magnitude => %.2f\n"
+	       "a + b => (%.2f, %.2f)\n"
+	       "a - b => (%.2f, %.2f)\n"
+	       "unit from a => (%.2f, %.2f)\n"
+	       "unit from b => (%.2f, %.2f)\n",
 	       a.x, a.y,
 	       b.x, b.y,
-	       a_magnitude,
-	       b_magnitude,
-	       a_to_b_diff.x, a_to_b_diff.y,
-	       b_to_a_diff.x, b_to_a_diff.y,
-	       diff_magnitude);
+	       a_mag, b_mag,
+	       diff.x, diff.y,
+	       negdiff.x, negdiff.y,
+	       diff_mag,
+	       ab_sum.x, ab_sum.y,
+	       ab_sub.x, ab_sub.y,
+	       a_unit.x, a_unit.y,
+	       b_unit.x, b_unit.y);
 
 
 	return EXIT_SUCCESS;
