40e5aac075bea0ec45f052a882d79b89294dd5c9
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-01-09 09:57:43 +0000

    refact 2d_vectors.c

--- c/study/graphics/2d_vectors.c
+++ c/study/graphics/2d_vectors.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <assert.h>
 
+static const float kPI = 3.141592;
+
 struct Vector2f {
 	float x, y;
 };
@@ -70,32 +72,51 @@ int main(int argc, char** argv)
 	const vec2f_t a = { strtof(argv[1], NULL), strtof(argv[2], NULL) };
 	const vec2f_t b = { strtof(argv[3], NULL), strtof(argv[4], NULL) };
 
+	// calculate the sum and difference
 	const vec2f_t ab_sum = add_vec2f(a, b);
 	const vec2f_t ba_sub = sub_vec2f(b, a);
 
+	// calculate the vectors length
 	const float a_len = len_vec2f(a);
 	const float b_len = len_vec2f(b);
 	const float diff_len = len_vec2f(ba_sub);
 
+	// calculate the vectors angle to each other
+	const float dot_prod = dot_vec2f(a, b);
+	const float radians = acosf(dot_prod / (a_len * b_len));
+	const float degrees = radians * (180 / kPI);
+
+	// this shall give the same result as above
+	// but with the unit vectors, we don't need divide by lengths multiplied,
+	// as their lengths must be equal to 1
 	const vec2f_t a_unit = unit_vec2f(a, a_len);
 	const vec2f_t b_unit = unit_vec2f(b, b_len);
+	const float unit_dot_prod = dot_vec2f(a_unit, b_unit);
+	const float unit_radians = acosf(unit_dot_prod);
+	const float unit_degrees = radians * (180 / kPI);
 
-	const float dot_prod = dot_vec2f(a, b);
-	const float radians = acos(dot_prod / (a_len * b_len));
-	const float degrees = radians * (180 / 3.141592);
 
-	printf("a => (%.2f, %.2f)\n"
+	printf("VECTORS:\n"
+	       "a => (%.2f, %.2f)\n"
 	       "b => (%.2f, %.2f)\n"
+	       "\nLENGTHS:\n"
 	       "||a|| => %.2f\n"
 	       "||b|| => %.2f\n"
+	       "\nSUM AND DIFFERENCE:\n"
 	       "a + b => (%.2f, %.2f)\n"
 	       "b - a => (%.2f, %.2f)\n"
 	       "||b - a|| => %.2f\n"
-	       "unit from a => (%.2f, %.2f)\n"
-	       "unit from b => (%.2f, %.2f)\n"
-	       "dot product between a and b => %.2f\n"
-	       "angle between a and b => %f radians\n"
-	       "angle between a and b => %.1f degrees\n",
+	       "\nUNITS:\n"
+	       "unit a => (%.2f, %.2f)\n"
+	       "unit b => (%.2f, %.2f)\n"
+	       "\nANGLE:\n"
+	       "dot product => %.2f\n"
+	       "radians => %f\n"
+	       "degrees => %.2f\n"
+	       "\nANGLE CALCULATED FROM A AND B UNITS:\n"
+	       "dot product => %.2f\n"
+	       "radians => %f\n"
+	       "degrees => %.2f\n",
 	       a.x, a.y,
 	       b.x, b.y,
 	       a_len, b_len,
@@ -104,7 +125,8 @@ int main(int argc, char** argv)
 	       diff_len,
 	       a_unit.x, a_unit.y,
 	       b_unit.x, b_unit.y,
-	       dot_prod, radians, degrees);
+	       dot_prod, radians, degrees,
+	       unit_dot_prod, unit_radians, unit_degrees);
 
 
 	return EXIT_SUCCESS;
