a5710f12c88a52e399eba671bc0c9053feeaed18
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-02-14 12:52:03 +0000

    prime uri judge

---
+++ c/problems/repetition/prime_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int ncases, i;
+	long number, j;
+	unsigned char is_prime;
+
+	scanf("%i", &ncases);
+
+	for (i = 0; i < ncases; ++i) {
+		scanf("%ld", &number);
+		
+		if (number < 2) {
+			is_prime = 0;
+		} else {
+			is_prime = 1;
+			for (j = 2; j < number; ++j) {
+				if ((number % j) == 0) {
+					is_prime = 0;
+					break;
+				}
+			}
+		}
+
+		printf("%ld %s primo\n", number, is_prime ? "eh" : "nao eh");
+	}
+}
+
