7c54642efd682d4dcaf574fd1a357ce34fc4dfbc
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-03-04 17:41:40 +0000

    line in array added

---
+++ c/problems/arrays/line_in_array.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+inline float sum(float array[12])
+{
+	int i;
+	float res = 0;
+	for (i = 0; i < 12; ++i)
+		res += array[i];
+
+	return res;
+}
+
+
+int main(void)
+{
+	int i;
+	int line;
+	char op;
+	float array[12];
+	float result = 0;
+
+	scanf("%d %c", &line, &op);
+
+	for (i = 0; i < line; ++i)
+		scanf("%*f %*f %*f %*f %*f %*f %*f %*f %*f %*f %*f %*f");
+
+	scanf("%f %f %f %f %f %f %f %f %f %f %f %f",
+	       &array[0], &array[1], &array[2], &array[3], &array[4], &array[5],
+	       &array[6], &array[7], &array[8], &array[9], &array[10], &array[11]);
+
+	switch (op) {
+		case 'S': result = sum(array); break;
+		case 'M': result = sum(array) / 12; break;
+	}
+
+	printf("%.1f\n", result);
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
