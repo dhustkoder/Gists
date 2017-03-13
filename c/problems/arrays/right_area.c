792a273a9695d647a6a9212367a03f82df7d9c8e
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-03-13 21:18:09 +0000

    Right Area Added
    rev

--- c/problems/arrays/right_area.c
+++ c/problems/arrays/right_area.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void)
 {
 	char op;
+	int i, j;
 	float value;
 	float res = 0;
-	int i, j;
 
 	scanf("%c", &op);
 
 	for (i = 0; i < 12; ++i) {
 
-		for (j = 0; j < (11 - i); ++j) {
+		if (i < 6) {
+			for (j = 0; j < (12 - i); ++j)
+				scanf("%*f");
+		} else {
+			for (j = 0; j < (i + 1); ++j)
+				scanf("%*f");
+		}
+
+		for ( ; j < 12; ++j) {
 			scanf("%f", &value);
 			res += value;
 		}
-
-		for (; j < 12; ++j)
-			scanf("%*f");
 	}
 
-	printf("%.1f\n", op == 'S' ? res : res / 66);
+	printf("%.1f\n", op == 'S' ? res : res / 30);
 
 	return EXIT_SUCCESS;
 }
