65d8bfe54a28a16eb84945abd5449fb6aadd6f36
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-03-23 19:41:32 +0000

    getline one fixed

--- c/problems/adhoc/getline_one.c
+++ c/problems/adhoc/getline_one.c
@@ -1,27 +1,31 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void)
 {
+	int dist;
+	int dist_sum = 0;
 	int divisor = 0;
-	float res = 0;
-	int input;
 
+	size_t size = 256;
+	char* buffer = malloc(sizeof(char) * size);
 
 	for (;;) {
-
-		if (scanf("%*[^\n]") == EOF)
+		if (getline(&buffer, &size, stdin) == -1)
 			break;
-		else if (scanf("%d\n", &input) == EOF)
+		else if (scanf("%d%*c", &dist) == EOF)
 			break;
 
-		res += input;
+		dist_sum += dist;
 		++divisor;
 	}
 
+	printf("%.1f\n", (float)dist_sum / (float)divisor);
 
-	printf("%.1f\n", res / divisor);
 
+	free(buffer);
 	return EXIT_SUCCESS;
 }
 
