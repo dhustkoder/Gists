93f4e8b3e2577feb5950ef6c9bb19b6db859cffe
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-03-27 16:48:13 +0000

    algorithms study revisions
    Binary Search Revision

---
+++ c/study/algorithms/binary_search.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static int compare(const void* const lhs, const void* const rhs)
+{
+	return (*(int*)lhs) > (*(int*)rhs);
+}	
+
+
+static int* make_list(const char* const* const strs, const int size)
+{
+	int* const res = malloc(sizeof(int) * size);
+
+	for (int i = 0; i < size; ++i)
+		res[i] = (int) strtol(strs[i], NULL, 0);
+
+	qsort(res, size, sizeof(int), compare);
+
+	return res;
+}
+
+
+static const int* binary_search(const int target, const int* const array, const int size)
+{
+	int min = 0;
+	int max = size - 1;
+	int guess;
+
+	while (min <= max) {
+		guess = (min + max) / 2;
+		if (array[guess] < target)
+			min = guess + 1;
+		else if (array[guess] > target)
+			max = guess - 1;
+		else
+			return &array[guess];
+	}
+
+	return NULL;
+}
+
+
+int main(const int argc, const char* const * const argv)
+{
+	if (argc < 3) {
+		printf("Usage: %s [list] [target]\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	const int target = (int) strtol(argv[argc - 1], NULL, 0);
+	const int size = argc - 2;
+	int* const array = make_list(&argv[1], size);
+
+	for (int i = 0; i < size; ++i)
+		printf("[%d] = %d\n", i, array[i]);
+
+
+	const int* const addr = binary_search(target, array, size);
+
+	if (addr)
+		printf("FOUND %d AT INDEX %d\n", target, (int) (addr - array));
+	else
+		printf("NOT FOUND!\n");
+
+	free(array);
+	return EXIT_SUCCESS;
+}
+
+
+
---
+++ c/study/algorithms/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$CC" != "" ]; then
+	$CC -std=c11 -Wall -Wextra -pedantic -O2 -lm -o "${1%".c"}.out" $1
+else
+	echo "Set your C compiler by CC environment variable. Example: export CC=gcc"
+fi
+
