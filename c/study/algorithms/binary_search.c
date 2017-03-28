a5c85cfaa656addb0c8bfbc4d8a13b2e8ef45c91
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-03-28 21:04:09 +0000

    common

--- c/study/algorithms/binary_search.c
+++ c/study/algorithms/binary_search.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-static int compare(const void* const lhs, const void* const rhs)
-{
-	return (*(int*)lhs) > (*(int*)rhs);
-}	
-
-
-static int* make_list(const char* const* const strs, const int size)
-{
-	int* const res = malloc(sizeof(int) * size);
-
-	for (int i = 0; i < size; ++i)
-		res[i] = (int) strtol(strs[i], NULL, 0);
-
-	qsort(res, size, sizeof(int), compare);
-
-	return res;
-}
-
+#include "common.h"
 
 static const int* binary_search(const int target, const int* const array, const int size)
 {
@@ -41,6 +22,12 @@ static const int* binary_search(const int target, const int* const array, const 
 }
 
 
+static int compare(const void* const lhs, const void* const rhs)
+{
+	return (*(int*)lhs) > (*(int*)rhs);
+}	
+
+
 int main(const int argc, const char* const * const argv)
 {
 	if (argc < 3) {
@@ -50,11 +37,11 @@ int main(const int argc, const char* const * const argv)
 
 	const int target = (int) strtol(argv[argc - 1], NULL, 0);
 	const int size = argc - 2;
-	int* const array = make_list(&argv[1], size);
+	int* const array = make_array_from_strings(&argv[1], size);
+	qsort(array, size, sizeof(int), compare);
 
 	for (int i = 0; i < size; ++i)
 		printf("[%d] = %d\n", i, array[i]);
-
 
 	const int* const addr = binary_search(target, array, size);
 
