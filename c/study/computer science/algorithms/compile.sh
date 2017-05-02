63af45ad47ab94ba05eb38793abed9167340bb4c
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-05-02 18:34:50 +0000

    rev

--- c/study/computer science/algorithms/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$CC" != "" ]; then
-	$CC -std=c11 -Wall -Wextra -pedantic -O2 -lm -o "${1%".c"}.out" $@
-else
-	echo "Set your C compiler by CC environment variable. Example: export CC=gcc"
-fi
-
