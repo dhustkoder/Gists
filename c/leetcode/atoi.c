d713c8fcf68f6451b74787a3b56ea83148435b54
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-05-05 16:01:40 +0000

    leetcode atoi refact

--- c/leetcode/atoi.c
+++ c/leetcode/atoi.c
@@ -5,44 +5,43 @@
 
 static int atoi(const char* const str)
 {
-	char aux;
-	int sign = 1;
+	int sign;
+	int64_t limit;
 	int64_t r = 0;
 	const char* p = str;
 
-	while ((*p < '0' || *p > '9') && *p != '\0')
+	while (*p == ' ')
 		++p;
 
-	if (*p == '\0')
-		return r;
-
-	if (p != str) {
-		aux = *(p - 1);
-		if (aux != '-' && aux != '+' && aux != ' ')
-			return r;
-		else if (aux == '-')
-			sign = -1;
-
-		if ((p - str) > 1) {
-			aux = *(p - 2);
-			if (aux != ' ' && aux != '\t')
-				return r;
-		}
+	if (*p == '-') {
+		limit = -((uint64_t)INT_MIN);
+		sign = -1;
+	} else {
+		limit = INT_MAX;
+		sign = 1;
 	}
 
-	for (; *p >= '0' && *p <= '9'; ++p)
+	if (*p == '-' || *p == '+')
+		++p;
+
+	for (; *p >= '0' && *p <= '9'; ++p) {
 		r = (r * 10) + (*p - '0');
+		if (r >= limit)
+			return sign > 0 ? INT_MAX : INT_MIN;
+	}
 
-	r *= sign;
-
-	return r > INT_MAX ? INT_MAX : r < INT_MIN ? INT_MIN : r;
+	return (int) (r * sign);
 }
-
 
 
 int main(void)
 {
-	const char* const strs[] = { "015", "-015", "123", "321", "-9999", "+9999", "+-2", "    010", "2147483648" };
+	const char* const strs[] = {
+		"015", "-015", "123", "321", "-9999",
+		"+9999", "+-2", "    010", "2147483648","  +  413",
+		"9223372036854775809", "-2147483649", "18446744073709551617"
+	};
+	
 	int i;
 
 	for (i = 0; i < (int)(sizeof(strs)/sizeof(strs[0])); ++i)
