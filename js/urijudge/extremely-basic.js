21d66a07044894793e8541060e686c7d3bef69a8
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-02-21 20:25:02 -0300

    bind this example

--- js/urijudge/extremely-basic.js
+++ js/urijudge/extremely-basic.js
@@ -1,6 +1,42 @@
 
 
-console.log('testing');
+class Test  {
+	constructor() 
+	{
+		console.log('test contructor');
+		this.testingVar = 'Hello Modafoka';
+		this.testMethod = this._testMethod.bind(this);
+	}
 
+	_testMethod()
+	{
+		this.testingVar = 'Testing Var Changed';
+	}
+	
+	printTestingVar()
+	{
+		console.log(this.testingVar);
+	}
+
+}
+
+
+class FunCaller {
+	constructor(fun)
+	{
+		this.fun = fun;
+	}
+
+	callFun()
+	{
+		this.fun();
+	}
+}
+
+
+test = new Test();
+funCaller = new FunCaller(test.testMethod);
+funCaller.callFun();
+test.printTestingVar();
 
 
