d56dbb4a1d195bb60105581a1e4977f18fb33fe3
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-02-25 08:21:22 -0300

    area of circle js

---
+++ js/urijudge/area-of-circle.js
@@ -0,0 +1,6 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+const pi = (Math.PI).toFixed(5);
+const r = parseFloat(lines[0]).toFixed(2);
+console.log((Math.pow(r, 2) * pi).toFixed(4));
+
