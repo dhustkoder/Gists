2cbcce7302025e021fa610003bf0caefe9e188cc
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-02-26 23:22:33 -0300

    sphere

--- js/urijudge/sphere.js
+++ js/urijudge/sphere.js
@@ -1,6 +1,7 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-const r3 = Math.pow(parseFloat(lines[0]), 3);
-console.log('VOLUME =', ((4/3.0) * Math.PI * r3).toFixed(3));
+const pi = 3.14159;
+const r3 = Math.pow(parseFloat(lines[0]), 3).toFixed(5);
+console.log('VOLUME =', ((4.0/3.0) * pi * r3).toFixed(3));
 
